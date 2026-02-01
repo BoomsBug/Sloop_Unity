e80baa2b823b71e649809f8cd8626eb7578b2f69
Noor Yousef <nooryousef003@gmail.com>
2026-02-01 14:35:02 -0800

    Wind Force Added to Boat

--- Sloop_Unity/Assets/Scripts/Sailing/BoatMovement.cs
+++ Sloop_Unity/Assets/Scripts/Sailing/BoatMovement.cs
@@ -9,9 +9,13 @@ public class BoatMovement : MonoBehaviour
     public float boatSpeed = 6.0f;
     private float boatAcceleration;
     bool accelerated = false;
+
     public Transform dockCheck;
     bool dockPressed = false;
     
+    public enum WindDir {E, NE, N, NW, W, SW, S, SE} // 8 directions: 4 cardinal and 4 intercardinal direction
+    public WindDir windDirection = WindDir.SW;
+    public float windStrength = 1.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +31,16 @@ public class BoatMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        Vector2 windVelocity = WindVectorDirection(windDirection) * windStrength;
+
         if (accelerated) {
             boatRigidbody.velocity = new Vector2(horizontalInput * boatAcceleration, verticalInput * boatAcceleration);
         }
         else {
             boatRigidbody.velocity = new Vector2(horizontalInput * boatSpeed, verticalInput * boatSpeed);
         } 
+
+        boatRigidbody.velocity += windVelocity; // add wind velocity to boat's
 
         if (boatRigidbody.velocity.x > 0 && transform.localScale.x < 0 ||
             boatRigidbody.velocity.x < 0 && transform.localScale.x > 0) { // if going in opposite direction
@@ -48,6 +56,42 @@ public class BoatMovement : MonoBehaviour
 
 
     }
+
+    // This function returns the wind vector direction based on the direction chosen
+    private Vector2 WindVectorDirection(WindDir dir) {
+        switch (dir) {
+
+            case WindDir.E:
+                return new Vector2(1f, 0f);
+
+            case WindDir.NE:
+                return new Vector2(1f, 1f).normalized;
+
+            case WindDir.N:
+                return new Vector2(0f, 1f);
+
+            case WindDir.NW:
+                return new Vector2(-1f, 1f).normalized;
+
+            case WindDir.W:
+                return new Vector2(-1f, 0f);
+
+            case WindDir.SW:
+                return new Vector2(-1f, -1f).normalized;
+
+            case WindDir.S:
+                return new Vector2(0f, -1f);
+
+            case WindDir.SE:
+                return new Vector2(1f, -1f).normalized;
+
+            default:
+                return new Vector2(-1f, -1f);
+
+        }
+    }
+
+
 
 
     // Update is called once per frame
