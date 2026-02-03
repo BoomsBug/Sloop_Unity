15e358915ab96cee6e3fc3ad9fdce290e2abfdde
BoomsBug <oomsie8@icloud.com>
2026-02-02 16:48:42 -0800

    Basic Game Manager
    Committing basic game manager with changes to be tested on current scenes

--- Sloop_Unity/Assets/Scripts/Managers/GameManager.cs
+++ Sloop_Unity/Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+
+
+// Used Skeleton from Tarodev's tutorial for initial setup
+// https://www.youtube.com/watch?v=4I0vonyqMi8
 
 public class GameManager : MonoBehaviour
 {
 
     public GameState state;
 
+    public static event Action<GameState> OnGameStateChanged;
+
 
     // GameManager instance to grab from anywhere in game
     public static GameManager Instance;
+
+    // One game manager only and game manager 
+    // continues from scene to scene
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
         Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+
+        // Starting game state
+        UpdateGameState(GameState.StartScreen);
+
+
     }
 
     public void UpdateGameState(GameState newState)
@@ -22,21 +49,68 @@ public class GameManager : MonoBehaviour
         switch (newState)
         {
             case GameState.StartScreen:
+                HandleStartScreen();
                 break;
-            case GameState.Sailing: 
+            case GameState.Sailing:
+                HandleSailing();
                 break;
             case GameState.Island:
+                HandleIsland();
                 break;
             case GameState.Port:
+                HandlePort();
                 break;
             case GameState.PauseMenu:
+                HandlePauseMenu();
                 break;
             case GameState.VictoryMenu:
+                HandleVictoryMenu();
                 break;
             case GameState.LossMenu:
+                HandleLossMenu();
                 break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
 
         }
+
+        // Has anything changed? If so, invoke newState 
+        OnGameStateChanged?.Invoke(newState);
+    }
+
+    private void HandleStartScreen()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    private void HandleSailing()
+    {
+        SceneManager.LoadScene("Sailing");
+    }
+
+    private void HandleIsland()
+    {
+        SceneManager.LoadScene("Island");
+    }
+
+    private void HandlePort()
+    {
+        SceneManager.LoadScene("Port");
+    }
+
+    private void HandlePauseMenu()
+    {
+        SceneManager.LoadScene("PauseMenu");
+    }
+
+    private void HandleVictoryMenu()
+    {
+        SceneManager.LoadScene("VictoryMenu");
+    }
+
+    private void HandleLossMenu()
+    {
+        SceneManager.LoadScene("LossMenu");
     }
 }
 
