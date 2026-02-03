51b3f85319fd27578e502b25dfb8c8a9d2617f61
AcaNEH <shaoxuancheng2003@gmail.com>
2026-02-02 20:42:07 -0800

    Interaction/Player Stats
    Implemented a very simple interaction system where the player switches color when interacting with an "NPC" or picking up an "Item", and added HP, FP, and Honor stats to the playerStats

---
+++ Sloop_Unity/Assets/Scripts/Player/SimpleNPC.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleItem : MonoBehaviour
+{
+    // Just a marker script, no extra functionality needed
+}
