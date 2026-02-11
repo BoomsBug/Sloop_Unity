45dc9b09f13e8a99e65b2cf31f094c06930d811a
AcaNEH <shaoxuancheng2003@gmail.com>
2026-02-10 17:57:07 -0800

    Revert "Simple Storage UI for wood, gold, food &  UI for spending gold at Merchants"
    This reverts commit 3d909bf1687b9b5c98d5534caa33ca1cec85c422.

--- Sloop_Unity/Assets/Scripts/Player/TopDownMovement.cs
+++ Sloop_Unity/Assets/Scripts/Player/TopDownMovement.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TopDownPlayerMovement : MonoBehaviour
 {
@@ -13,15 +12,7 @@ public class TopDownPlayerMovement : MonoBehaviour
     public Color defaultColor = Color.blue;
     public Color npcInteractionColor = Color.green;
     public Color itemPickupColor = Color.yellow;
-    public Color merchantInteractionColor = Color.green;
     
-    // UI References
-    public GameObject merchantPanel;
-    public Text foodPriceText;
-    public Text woodPriceText;
-    public Text foodAmountText;
-    public Text woodAmountText;
-
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private Vector2 moveInput;
@@ -30,53 +21,25 @@ public class TopDownPlayerMovement : MonoBehaviour
     // Track what we're near
     private bool isNearNPC = false;
     private bool isNearItem = false;
-    private bool isNearMerchant = false;
-
-    private PlayerResources playerResources;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        playerResources = GetComponent<PlayerResources>();
         rb.gravityScale = 0;
         spriteRenderer.color = defaultColor;
-        
-        // Hide merchant UI initially
-        if (merchantPanel != null)
-            merchantPanel.SetActive(false);
     }
+
     void Update()
     {
         moveInput.x = Input.GetAxis("Horizontal");
         moveInput.y = Input.GetAxis("Vertical");
         
         // Handle interactions
-        if (Input.GetKeyDown(KeyCode.E) && isNearMerchant)
-        {
-            if (spriteRenderer.color == defaultColor)
-            {
-                // Open merchant UI
-                spriteRenderer.color = merchantInteractionColor;
-                OpenMerchantUI();
-            }
-            else if (spriteRenderer.color == merchantInteractionColor)
-            {
-                // Close merchant UI
-                spriteRenderer.color = defaultColor;
-                CloseMerchantUI();
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.E) && isNearNPC && spriteRenderer.color == defaultColor)
+        if (Input.GetKeyDown(KeyCode.E) && isNearNPC)
         {
             spriteRenderer.color = npcInteractionColor;
             Debug.Log("Talked to NPC");
-        } 
-        else if (Input.GetKeyDown(KeyCode.E) && isNearNPC && spriteRenderer.color == npcInteractionColor)
-        {
-            spriteRenderer.color = defaultColor;
-            Debug.Log("Stopped talking to NPC");
         }
         
         if (Input.GetKeyDown(KeyCode.F) && isNearItem)
@@ -86,10 +49,9 @@ public class TopDownPlayerMovement : MonoBehaviour
         }
         
         // Reset to default color when moving away
-        if (moveInput.magnitude > 0.1f && !isNearNPC && !isNearItem && !isNearMerchant)
+        if (moveInput.magnitude > 0.1f && !isNearNPC && !isNearItem)
         {
             spriteRenderer.color = defaultColor;
-            CloseMerchantUI();
         }
     }
 
@@ -100,68 +62,8 @@ public class TopDownPlayerMovement : MonoBehaviour
             moveInput.magnitude > 0.1f ? 1f / acceleration : 1f / deceleration);
     }
     
-    void OpenMerchantUI()
-    {
-        if (merchantPanel != null)
-        {
-            merchantPanel.SetActive(true);
-            UpdateMerchantPrices();
-            Debug.Log("Merchant UI opened");
-        }
-    }
-    
-    void CloseMerchantUI()
-    {
-        if (merchantPanel != null)
-        {
-            merchantPanel.SetActive(false);
-            Debug.Log("Merchant UI closed");
-        }
-    }
-    
-    void UpdateMerchantPrices()
-    {
-        if (playerResources != null && merchantPanel.activeSelf)
-        {
-            int foodCost = playerResources.crewMates * 2;
-            int foodAmount = playerResources.crewMates * 3;
-            int woodCost = playerResources.shipLevel * 5;
-            int woodAmount = playerResources.shipLevel * 4;
-            
-            if (foodPriceText != null)
-                foodPriceText.text = "Cost: " + foodCost + " Gold";
-            if (foodAmountText != null)
-                foodAmountText.text = "Get: " + foodAmount + " Food";
-            if (woodPriceText != null)
-                woodPriceText.text = "Cost: " + woodCost + " Gold";
-            if (woodAmountText != null)
-                woodAmountText.text = "Get: " + woodAmount + " Wood";
-        }
-    }
-    
-    // Public methods for UI buttons to call
-    public void BuyFood()
-    {
-        if (playerResources != null && playerResources.BuyFoodFromMerchant())
-        {
-            UpdateMerchantPrices();
-        }
-    }
-    
-    public void BuyWood()
-    {
-        if (playerResources != null && playerResources.BuyWoodFromMerchant())
-        {
-            UpdateMerchantPrices();
-        }
-    }
-    
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Merchant"))
-        {
-            isNearMerchant = true;
-        }
         if (other.CompareTag("NPC"))
         {
             isNearNPC = true;
@@ -174,12 +76,6 @@ public class TopDownPlayerMovement : MonoBehaviour
     
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Merchant"))
-        {
-            isNearMerchant = false;
-            spriteRenderer.color = defaultColor;
-            CloseMerchantUI();
-        }
         if (other.CompareTag("NPC"))
         {
             isNearNPC = false;
