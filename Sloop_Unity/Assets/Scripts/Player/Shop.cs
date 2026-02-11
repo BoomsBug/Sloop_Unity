9fd16ad2e677cc45786012edcdd497c84e8fada4
AcaNEH <shaoxuancheng2003@gmail.com>
2026-02-10 18:57:41 -0800

    Update Shop.cs

--- Sloop_Unity/Assets/Scripts/Player/Shop.cs
+++ Sloop_Unity/Assets/Scripts/Player/Shop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Merchant : MonoBehaviour
+public class shop : MonoBehaviour
 {
     public enum MerchantType
     {
