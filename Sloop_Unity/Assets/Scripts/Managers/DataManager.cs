2bedc1ab8553dd2c0d99bc71b12aa22be59a9178
BoomsBug <oomsie8@icloud.com>
2026-02-25 18:56:16 -0800

    Updated DataManager and DataStorage Scripts
    Added most of the current data that we need to save in the game. DataStorage now has the data from unity to fetch for saving. DataManager uses DataStorage to collect data, save to json, and then load data back. Still need to debug, add more things to be saved, and link to proper UI in unity.

--- Sloop_Unity/Assets/Scripts/Managers/DataManager.cs
+++ Sloop_Unity/Assets/Scripts/Managers/DataManager.cs
@@ -4,24 +4,62 @@ using System.IO;
 using UnityEngine;
 
 // used info from https://www.youtube.com/watch?v=wJhMaLkeEuY for initial skeleton
+// used Chat GPT for assistance and debugging help after I identified what needed to be saved
 
 public class DataManager : MonoBehaviour
 {
 
     public Transform playerTransform;
 
-
     // save game function
     public void SaveGame()
     {
         // new instance of class
-        DataStorage dataStorage = new DataStorage();
-        
-        // new float array
-        dataStorage.position = new float[] { playerTransform.position.x, playerTransform.position.y };
+        DataStorage data = new DataStorage();
+
+
+        ///
+
+        // World data
+        data.gameState = GameManager.Instance.state;
+        data.worldGenSeed = GameManager.Instance.worldSeed;
+
+
+        // Sloop-O-War data
+        data.boatPosition = new float[]
+        {
+            GameManager.Instance.boatPosition.x,
+            GameManager.Instance.boatPosition.y,
+            GameManager.Instance.boatPosition.z
+        };
+
+        data.boatVelocity = new float[] {
+        GameManager.Instance.boatVelocity.x,
+        GameManager.Instance.boatVelocity.y
+        };
+
+        data.hasBoatState = GameManager.Instance.hasBoatState;
+
+
+        // Island data
+        data.currentIslandID = GameManager.Instance.currentIslandID;
+        data.currentIsalndMorality = GameManager.Instance.currentIslandMorality;
+
+
+
+        // Player data
+        data.playerPosition = new float[]
+        {
+            playerTransform.position.x,
+            playerTransform.position.y,
+            playerTransform.position.z
+        };
+
+        ///
+
 
         // datastorage to json string
-        string json = JsonUtility.ToJson(dataStorage);
+        string json = JsonUtility.ToJson(data);
 
         // builds a file path for json string
         string path = Application.persistentDataPath + "/saveData.json";
@@ -29,6 +67,14 @@ public class DataManager : MonoBehaviour
         // writes json string into the file
         System.IO.File.WriteAllText(path, json);
     }
+
+
+
+
+
+
+
+
 
 
     // load game function 
@@ -44,10 +90,45 @@ public class DataManager : MonoBehaviour
             string json = System.IO.File.ReadAllText(path);
             DataStorage loadedData = JsonUtility.FromJson<DataStorage>(json);
 
-            // update the player position
-            playerTransform.position = new Vector2(loadedData.position[0], loadedData.position[1]);
-            Vector3 loadedPosition = new Vector2(loadedData.position[0], loadedData.position[1]);
+
+            ///
+
+            // World data loaded back in
+            GameManager.Instance.worldSeed = loadedData.worldGenSeed;
+            GameManager.Instance.UpdateGameState(loadedData.gameState); // Update game state with save after seed generated
+
+
+            // Sloop-O-War data loaded back in
+            GameManager.Instance.boatPosition = new Vector3
+            (
+            loadedData.boatPosition[0],
+            loadedData.boatPosition[1],
+            loadedData.boatPosition[2]
+            );
+
+            GameManager.Instance.boatVelocity = new Vector2(
+            loadedData.boatVelocity[0],
+            loadedData.boatVelocity[1]
+            );
+
+            GameManager.Instance.hasBoatState = loadedData.hasBoatState;
+
+
+            // Island data loaded back in
+            GameManager.Instance.currentIslandID = loadedData.currentIslandID;
+            GameManager.Instance.currentIslandMorality = loadedData.currentIsalndMorality;
+
+
+
+            // Player data loaded back in
+            Vector3 loadedPosition = new Vector3 (
+            loadedData.playerPosition[0],
+            loadedData.playerPosition[1],
+            loadedData.playerPosition[2]
+            );
             playerTransform.position = loadedPosition;
+
+            ///
 
         }
         else
--- Sloop_Unity/Assets/Scripts/Managers/DataStorage.cs
+++ Sloop_Unity/Assets/Scripts/Managers/DataStorage.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 [System.Serializable]
 
-// Class for in game data
+// Class for in game data which should be saved
 public class DataStorage
 {
-    public float[] position;
+    // World data
+    public GameState gameState;
+    public int worldGenSeed;
+
+    // Sloop-O-War data
+    public float[] boatPosition;
+    public float[] boatVelocity;
+    public bool hasBoatState;
+
+    // Island data
+    public int currentIslandID;
+    public string currentIsalndMorality;
+
+    // Player data
+    public float[] playerPosition;
+
+
 }
