a263f9400bcfcf0cecce35469cf6d841531a60fd
LurdVoldemort <drwhostinson@icloud.com>
2026-03-05 16:00:18 -0800

    added integration with Andrew's resource system
    checks if player can afford an option, then subtracts/adds relevant resources.

--- Sloop_Unity/Assets/Scripts/EncounterSystem/EncounterOptionSO.cs
+++ Sloop_Unity/Assets/Scripts/EncounterSystem/EncounterOptionSO.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "EncounterOption", menuName = "ScriptableObjects/Encounter/Option", order = 1)]
 public class EncounterOptionSO : ScriptableObject
 {
-        //list of resources: [gold, food, power, morality]
+        //list of resources: [gold, wood, food, power, honour]
     [TextArea] public string text;
-    public int[] cost = new int[4];
-    public int[] gain = new int[4];
+    [Tooltip("[gold, wood, food, power, honour]")]
+    public int[] cost = new int[5];
+    [Tooltip("[gold, wood, food, power, honour]")]
+    public int[] gain = new int[5];
     public bool isGainHidden;
     [TextArea] public string outcome;
 }
--- Sloop_Unity/Assets/Scripts/EncounterSystem/EncounterSystem.cs
+++ Sloop_Unity/Assets/Scripts/EncounterSystem/EncounterSystem.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+//using System.Resources;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using Sloop.Economy;
+using UnityEngine.UI;
 
 public class EncounterSystem : MonoBehaviour
 {
     public GameObject encounterUI;
-    public List<ScriptableObject> possibleEncounters;
-    public EncounterSO nextEncounter;
+    public List<EncounterSO> possibleEncounters;
+    private List<EncounterSO> completedEncounters = new List<EncounterSO>();
+    public EncounterSO curEncounter;
     public GameObject continueButton;
     private bool canContinue;
     private bool canChoose;
@@ -19,7 +23,7 @@ public class EncounterSystem : MonoBehaviour
     public TextMeshProUGUI encounterText;
     public List<GameObject> optionPanels;
 
-    public void LoadNextEncounter()
+    public void LoadEncounter()
     {
         //ensures only one encounter at a time
         if (isEncounterActive) return;
@@ -32,13 +36,13 @@ public class EncounterSystem : MonoBehaviour
         isEncounterActive = true;
         canChoose = true;
 
-        encounterText.text = nextEncounter.text;
+        encounterText.text = curEncounter.text;
 
         //ensures continue button is disabled
         continueButton.SetActive(false);
         canContinue = false;
         
-        for (int i = 0; i < optionPanels.Count; i ++)
+        for (int i = 0; i < curEncounter.options.Count; i ++)
         {
             GameObject panel = optionPanels[i];
 
@@ -46,31 +50,57 @@ public class EncounterSystem : MonoBehaviour
             panel.SetActive(true);
 
             //finds the right child object with tmp component and loads in correct text
-            panel.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = nextEncounter.options[i].text;
+            panel.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = curEncounter.options[i].text;
             //for each different resource, if that count is >0, adds it to the cost string
             string cost = $"";
-            if (nextEncounter.options[i].cost[0] > 0) cost += $"{nextEncounter.options[i].cost[0]}G "; //gold
-            if (nextEncounter.options[i].cost[1] > 0) cost += $"{nextEncounter.options[i].cost[1]}F "; //food
-            if (nextEncounter.options[i].cost[2] > 0) cost += $"{nextEncounter.options[i].cost[2]}P "; //power
-            if (nextEncounter.options[i].cost[3] > 0) cost += $"{nextEncounter.options[i].cost[3]}H"; //honour
+            if (curEncounter.options[i].cost[0] > 0) cost += $"{curEncounter.options[i].cost[0]}G "; //gold
+            if (curEncounter.options[i].cost[1] > 0) cost += $"{curEncounter.options[i].cost[1]}W "; //wood
+            if (curEncounter.options[i].cost[2] > 0) cost += $"{curEncounter.options[i].cost[2]}F "; //food
+            if (curEncounter.options[i].cost[3] > 0) cost += $"{curEncounter.options[i].cost[3]}P "; //power
+            if (curEncounter.options[i].cost[4] > 0) cost += $"{curEncounter.options[i].cost[4]}H";  //honour
             string gain = $"";
-            if (nextEncounter.options[i].gain[0] > 0) gain += $"{nextEncounter.options[i].gain[0]}G "; //gold
-            if (nextEncounter.options[i].gain[1] > 0) gain += $"{nextEncounter.options[i].gain[1]}F "; //food
-            if (nextEncounter.options[i].gain[2] > 0) gain += $"{nextEncounter.options[i].gain[2]}P "; //power
-            if (nextEncounter.options[i].gain[3] > 0) gain += $"{nextEncounter.options[i].gain[3]}H"; //honour
+            if (curEncounter.options[i].gain[0] > 0) gain += $"{curEncounter.options[i].gain[0]}G "; //gold
+            if (curEncounter.options[i].gain[1] > 0) gain += $"{curEncounter.options[i].gain[1]}W "; //wood
+            if (curEncounter.options[i].gain[2] > 0) gain += $"{curEncounter.options[i].gain[2]}F "; //food
+            if (curEncounter.options[i].gain[3] > 0) gain += $"{curEncounter.options[i].gain[3]}P "; //power
+            if (curEncounter.options[i].gain[4] > 0) gain += $"{curEncounter.options[i].gain[4]}H";  //honour
 
             panel.transform.Find("Cost").GetComponent<TextMeshProUGUI>().text = cost;
             panel.transform.Find("Gain").GetComponent<TextMeshProUGUI>().text = gain;
 
             //If option gain is hidden, replaces gain and altered gain with ???
-            if (nextEncounter.options[i].isGainHidden)
+            if (curEncounter.options[i].isGainHidden)
             {
                 panel.transform.Find("Gain").GetComponent<TextMeshProUGUI>().text = "???";
                 panel.transform.Find("Altered Gain").GetComponent<TextMeshProUGUI>().text = "???";
             }
 
-            //TODO: calls functions to calculate altered cost and gain based on hired crew;
+            //calls functions to calculate altered cost and gain based on hired crew;
+            int[] alteredCost = CalculateAlteredResources(curEncounter.options[i].cost);
+            int[] alteredGain = CalculateAlteredResources(curEncounter.options[i].gain);
 
+            string alteredCostString = $"";
+            if (alteredCost[0] > 0) alteredCostString += $"{alteredCost[0]}G "; //gold
+            if (alteredCost[1] > 0) alteredCostString += $"{alteredCost[1]}G "; //wood
+            if (alteredCost[2] > 0) alteredCostString += $"{alteredCost[2]}G "; //food
+            if (alteredCost[3] > 0) alteredCostString += $"{alteredCost[3]}G "; //power
+            if (alteredCost[4] > 0) alteredCostString += $"{alteredCost[4]}G";  //honour
+            string alteredGainString = $"";
+            if (alteredGain[0] > 0) alteredGainString += $"{alteredGain[0]}G "; //gold
+            if (alteredGain[1] > 0) alteredGainString += $"{alteredGain[1]}G "; //wood
+            if (alteredGain[2] > 0) alteredGainString += $"{alteredGain[2]}G "; //food
+            if (alteredGain[3] > 0) alteredGainString += $"{alteredGain[3]}G "; //power
+            if (alteredGain[4] > 0) alteredGainString += $"{alteredGain[4]}G";  //honour
+            
+            panel.transform.Find("Altered Cost").GetComponent<TextMeshProUGUI>().text = alteredCostString;
+            panel.transform.Find("Altered Gain").GetComponent<TextMeshProUGUI>().text = alteredGainString;
+
+            //Based on altered cost, shades button red if player cannot afford it
+            IEnumerable<ResourceAmount> alteredResourceCosts = BasicCosts2ResourceCosts(alteredCost);
+            if (!ResourceManager.Instance.CanAfford(alteredResourceCosts))
+            {
+                panel.GetComponent<Image>().color = Color.red;
+            }
         }
     }
 
@@ -78,25 +108,51 @@ public class EncounterSystem : MonoBehaviour
     {
         if (!canChoose) return;
 
-        //TODO: detect if player has enough resources to choose this option
+        //Detects if player has enough resources to choose this option
+        int[] optionCosts = curEncounter.options[option-1].cost;
 
-        encounterText.text = nextEncounter.options[option - 1].outcome;
-        //disable and hide option panels
+        //(re)calculates altered cost based on hired crew (same as above)
+        optionCosts = CalculateAlteredResources(optionCosts);
+
+        //then converts int[] to IEnumrable<ResourceAmount>
+        IEnumerable<ResourceAmount> resourceCosts = BasicCosts2ResourceCosts(optionCosts);
+
+        //if cant afford option, then return
+        if (!ResourceManager.Instance.CanAfford(resourceCosts)) {
+            return;
+        }
+
+        encounterText.text = curEncounter.options[option - 1].outcome;
+        //disables and hides option panels
         foreach (GameObject panel in optionPanels)
         {
             panel.SetActive(false);
             canChoose = false;
         }
 
-        //TODO: Take resources away from player
+        //Takes resources away from player
+        ResourceManager.Instance.TrySpend(resourceCosts);
 
-        //TODO: Add gained resources to player
+        //Adds gained resources to player (also calculate altered gains)
+        int[] optionGains = curEncounter.options[option-1].gain;
+        optionGains = CalculateAlteredResources(optionGains);
+        IEnumerable<ResourceAmount> resourceGains = BasicCosts2ResourceCosts(optionGains);
+        ResourceManager.Instance.Add(resourceGains);
 
-        //Enable continue button
+        //Enables continue button
         continueButton.SetActive(true);
         canContinue = true;
 
-        //TODO: decide what next encounter is
+        //Decides what next encounter is
+        completedEncounters.Add(curEncounter);
+        possibleEncounters.Remove(curEncounter);
+        //If all possible encounters used up, replenish list with completed encounters
+        if (possibleEncounters.Count <= 0)
+        {
+            possibleEncounters = new List<EncounterSO>(completedEncounters);
+            completedEncounters = new List<EncounterSO>();
+        }
+        curEncounter = possibleEncounters[Random.Range(0, possibleEncounters.Count)];
 
     }
 
@@ -113,6 +169,26 @@ public class EncounterSystem : MonoBehaviour
         isEncounterActive = false;
     }
 
+    private IEnumerable<ResourceAmount> BasicCosts2ResourceCosts(int[] costs)
+    {
+        //list of resources: [gold, wood, food, power, honour]
+        ResourceAmount[] cost =
+        {  
+            new ResourceAmount{type=Resource.Gold, amount=costs[0]},
+            new ResourceAmount{type=Resource.Wood, amount=costs[1]},
+            new ResourceAmount{type=Resource.Food, amount=costs[2]},
+            new ResourceAmount{type=Resource.Power, amount=costs[3]},
+            new ResourceAmount{type=Resource.Honour, amount=costs[4]}
+        };
+        return cost;
+    }
+
+    private int[] CalculateAlteredResources(int[] baseResources)
+    {
+        //TODO
+        return baseResources;
+    }
+
     void Update()
     {
         if      (Input.GetKeyDown(KeyCode.Alpha1)) ChooseOption(1);
@@ -120,12 +196,12 @@ public class EncounterSystem : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.Alpha3)) ChooseOption(3);
         else if (Input.GetKeyDown(KeyCode.Alpha4)) ChooseOption(4);
 
-        if (Input.GetKeyDown(KeyCode.C)) LoadNextEncounter();
+        if (Input.GetKeyDown(KeyCode.C)) LoadEncounter();
         if (Input.GetKeyDown(KeyCode.Space)) ContinueGame();
     } 
 
     void Start()
     {
-        LoadNextEncounter();
+        LoadEncounter();
     }
 }
