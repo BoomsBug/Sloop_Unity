c7f85260b29cb3ed09cd39e6349ce897896098a3
BoomsBug <oomsie8@icloud.com>
2026-03-10 20:50:42 -0700

    Cannon Minigame Working
    Added cannonball prefab, cannonmanager script for mouse tracking and firing, cannonballscript for cannon impacts, and particle system to simulate explosions on impact.

---
+++ Sloop_Unity/Assets/Scripts/Minigame Scripts/CannonBallScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Simple script that destroys cannonball after 5 seconds or on collision with
+// ocean. Also, instantiates an explosion particle system prefab on collision with 
+// Keg objects in scene.
+public class CannonBallScript : MonoBehaviour
+{
+    public GameObject ExplosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 5f); // Destroy after 5 seconds regardless
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ( collision.gameObject.CompareTag("Ocean"))
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Keg"))
+        {
+            Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
+
+            Rigidbody2D kegRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            
+            // Direction from cannonball to keg to get force dir 
+            Vector2 forceDir = (collision.transform.position - transform.position).normalized;
+
+            // Apply impulse force in force dir
+            kegRb.AddForce(forceDir * 100f, ForceMode2D.Impulse);
+
+            Destroy(gameObject);
+        }
+
+    }
+}
