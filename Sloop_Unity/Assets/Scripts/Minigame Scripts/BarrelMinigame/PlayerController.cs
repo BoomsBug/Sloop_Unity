c17bc06e9ed3a023028633d6db1caf3244c6ac8d
AcaNEH <shaoxuancheng2003@gmail.com>
2026-03-18 18:04:31 -0700

    Updated Barrel Mini Game
    Added hard and easy difficulty, added the ability to parry air projectiles while jumping

--- Sloop_Unity/Assets/Scripts/Minigame Scripts/BarrelMinigame/PlayerController.cs
+++ Sloop_Unity/Assets/Scripts/Minigame Scripts/BarrelMinigame/PlayerController.cs
@@ -1,22 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 7f;
+    public float parryRange = 2f;          // How close a cannon must be to parry
+    public int parryScore = 20;             // Points awarded for a successful parry
+    public float parryCooldown = 0.5f;      // Seconds between parries
 
     private Rigidbody2D rb;
     private bool isGrounded;
     private BarrelDashTestManager manager;
+    private float lastParryTime = -10f;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.freezeRotation = true; // Keep player upright
-
-        // Find manager (works even if player is disabled)
+        rb.freezeRotation = true;
         manager = FindObjectOfType<BarrelDashTestManager>();
     }
 
@@ -28,6 +28,12 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             isGrounded = false;
         }
+
+        // Parry input (Left Shift)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Time.time >= lastParryTime + parryCooldown)
+        {
+            TryParry();
+        }
     }
 
     void FixedUpdate()
@@ -36,13 +42,43 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
     }
 
+    void TryParry()
+    {
+        // Find all flying cannons within parry range
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, parryRange, LayerMask.GetMask("Default")); // Adjust layer if needed
+        FlyingCannon nearest = null;
+        float minDist = float.MaxValue;
+
+        foreach (var hit in hits)
+        {
+            if (hit.CompareTag("FlyingCannon"))
+            {
+                float dist = Vector2.Distance(transform.position, hit.transform.position);
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    nearest = hit.GetComponent<FlyingCannon>();
+                }
+            }
+        }
+
+        if (nearest != null)
+        {
+            nearest.Parry();
+            lastParryTime = Time.time;
+            if (manager != null)
+                manager.AddParryScore(parryScore);
+            Debug.Log("Parried a cannon!");
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
-        else if (collision.gameObject.CompareTag("Barrel"))
+        else if (collision.gameObject.CompareTag("Barrel") || collision.gameObject.CompareTag("FlyingCannon"))
         {
             if (manager != null)
                 manager.GameOver();
@@ -59,7 +95,6 @@ public class PlayerController : MonoBehaviour
 
     public void ResetPlayer()
     {
-        // Safety check in case manager wasn't found
         if (manager == null)
             manager = FindObjectOfType<BarrelDashTestManager>();
 
