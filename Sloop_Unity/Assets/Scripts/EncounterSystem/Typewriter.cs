d9225b3485139b8304a17e2ad7e6b218664adbe2
LurdVoldemort <drwhostinson@icloud.com>
2026-03-25 19:08:24 -0700

    finished encounter UI polishes

--- Sloop_Unity/Assets/Scripts/EncounterSystem/Typewriter.cs
+++ Sloop_Unity/Assets/Scripts/EncounterSystem/Typewriter.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class Typewriter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text textComponent; 
+    public float letterDelay = 0.1f;
+    private int totalCharacters;
+    private bool typewriterActive;
+
     void Start()
     {
-        
+        textComponent = GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
+    public void StartTypewriter()
+    {
+        totalCharacters = textComponent.text.Length;
+        textComponent.maxVisibleCharacters = 0;
+        typewriterActive = true;
+
+        StartCoroutine(Delay(letterDelay));
+    }
+
     void Update()
     {
-        
+        //for skipping to completed text
+        if (!typewriterActive) return;
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            textComponent.maxVisibleCharacters = totalCharacters;
+            typewriterActive = false;
+        }
+    }
+
+    IEnumerator Delay(float time)
+    {
+        while (textComponent.maxVisibleCharacters <= totalCharacters)
+        {
+            textComponent.maxVisibleCharacters ++;
+
+            yield return new WaitForSecondsRealtime(time);
+        }
+        typewriterActive = false;
     }
 }
